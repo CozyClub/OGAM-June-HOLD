3e687d3de96cb00cd9e017db49c1be57dc9c4caf
radmint <pdbystrom@gmail.com>
2021-06-13 19:07:27 -0700

    adding settings screen, buttons to start game and quit game

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button startButton, settingsButton, quitButton, backButton;
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartOnClick);
+        settingsButton.onClick.AddListener(SettingsOnClick);
+        quitButton.onClick.AddListener(QuitOnClick);
+        backButton.onClick.AddListener(BackOnClick);
+    }
+
+    void StartOnClick()
+    {
+        LoadScreen.LoadGameScene();
+    }
+
+    void SettingsOnClick()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    void QuitOnClick()
+    {
+        // TODO: confirmation modal
+        Debug.Log("Quitting game.");
+        Application.Quit();
+    }
+
+    void BackOnClick()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
